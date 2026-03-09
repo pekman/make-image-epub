e1219d3966f49c558888a3cc990bae26daffbb37
Pekka Ekman <pekman@users.noreply.github.com>
2026-03-09 13:30:46 +0200

    Add EPUB spec conformance test

---
+++ test/epub-spec-conformance.test.ts
@@ -0,0 +1,43 @@
+import epubchecker from "epubchecker";
+import { createWriteStream } from "node:fs";
+import { glob, mkdtempDisposable } from "node:fs/promises";
+import os from "node:os";
+import path from "node:path";
+import { Writable } from "node:stream";
+import { test } from "vitest";
+
+import { ImageReader } from "../src/cli/image-reader.js";
+import { makeEpub } from "../src/make-epub.js";
+
+
+test("EPUB spec conformance", async () => {
+  await using tmpdir = await mkdtempDisposable(
+    path.join(os.tmpdir(), "make-image-epub-test-"),
+  );
+  const testEpubPath = path.join(tmpdir.path, "test.epub");
+
+  const imageReaders = await Array.fromAsync(
+    glob(path.join(import.meta.dirname, "data", "*.png")),
+    (path) => new ImageReader(path),
+  );
+  await makeEpub(
+    imageReaders,
+    {
+      title: "test title",
+      language: "en",
+    },
+    Writable.toWeb(createWriteStream(testEpubPath, { flags: "wx" })),
+  );
+
+  const report = await epubchecker(testEpubPath, {
+    includeWarnings: true,
+    includeNotices: true,
+  });
+
+  const { nFatal, nError, nWarning } = report.checker;
+  if (nFatal !== 0 || nError !== 0 || nWarning !== 0) {
+    console.log("Messages from epubcheck:");
+    console.dir(report.messages, { depth: Infinity });
+    throw new Error("Conformance check failed");
+  }
+});
